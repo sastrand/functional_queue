60977e6c6decab473712dc969fc0fe6318b0dfac
Sascha Strand <sastrand@pdx.edu>
2018-03-12 11:13:22 -0700

    const time: empty, isEmpty)

--- main.hs
+++ main.hs
@@ -1,16 +1,15 @@
 module Main (main) where
-import Queue (Queue(..), empty, isEmpty, dequeue, enqueue)
+import Queue (empty, isEmpty)
 
 main :: IO()
 main = 
     do 
-        let q = Queue [1,2,3]
-        let r = enqueue 4 q
-        print r
-        print $ dequeue r
-        let s = empty
-        let t = enqueue 8 s
-        print $ isEmpty s
-        print $ isEmpty t
-        print t
-        print $ isEmpty $ snd $ dequeue t
+        let q = empty
+        print $ isEmpty q
+
+        -- let s = empty
+        -- let t = enqueue 8 s
+        -- print $ isEmpty s
+        -- print $ isEmpty t
+        -- print t
+        -- print $ isEmpty $ snd $ dequeue t
--- queue.hs
+++ queue.hs
@@ -1,21 +1,22 @@
-module Queue (Queue(..), empty, isEmpty, dequeue, enqueue) where
+module Queue (empty, isEmpty) where
 
-data Queue a = Queue [a] deriving (Show)
+data Queue a = Queue ([a], [a]) deriving (Show)
 
-empty :: Queue a
-empty = Queue([])
+-- empty :: Queue a
+empty = Queue([], [])
 
-isEmpty :: Queue a -> Bool
+-- isEmpty :: Queue a -> Bool
 isEmpty x =
     case x of 
-        Queue []     -> True
-        Queue (x:_)  -> False
+        Queue ([], [])     -> True
+        Queue ([], (x:_))  -> False
+        Queue ((x:_), [])  -> False
 
-frontOf :: Queue a -> a
-frontOf (Queue (x:xs)) = x
+-- frontOf :: Queue a -> a
+-- frontOf (Queue (x:xs)) = x
 
-dequeue :: Queue a -> (a, Queue a)
-dequeue (Queue xs) = (head xs, Queue $ tail xs)
+-- dequeue :: Queue a -> (a, Queue a)
+-- dequeue (Queue xs) = (head xs, Queue $ tail xs)
 
-enqueue :: a -> Queue a -> Queue a
-enqueue x (Queue xs) = Queue (xs ++ [x])
+-- enqueue :: a -> Queue a -> Queue a
+-- enqueue x (Queue xs) = Queue (xs ++ [x])
