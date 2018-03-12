ba84da63d1ee92f296a9358a632744941bbf2d29
Sascha Strand <sastrand@pdx.edu>
2018-03-12 11:46:59 -0700

    const time: enqueue, dequeue, isEmpty

--- main.hs
+++ main.hs
@@ -1,11 +1,15 @@
 module Main (main) where
-import Queue (empty, isEmpty)
+import Queue (empty, isEmpty, dequeue, enqueue)
 
 main :: IO()
 main = 
     do 
         let q = empty
         print $ isEmpty q
+        let r = enqueue 7 q
+        print $ isEmpty r
+        print $ dequeue r
+        print $ dequeue $ snd $ dequeue r
 
         -- let s = empty
         -- let t = enqueue 8 s
--- queue.hs
+++ queue.hs
@@ -1,22 +1,28 @@
-module Queue (empty, isEmpty) where
+module Queue (empty, isEmpty, dequeue, enqueue) where
 
 data Queue a = Queue ([a], [a]) deriving (Show)
 
 empty :: Queue a
 empty = Queue([], [])
 
+
 isEmpty :: Queue a -> Bool
-isEmpty x =
-    case x of 
-        Queue ([], [])     -> True
-        Queue ([], (x:_))  -> False
-        Queue ((x:_), [])  -> False
+isEmpty q =
+    case q of 
+        Queue ([], [])  -> True
+        Queue ([], xs)  -> False
+        Queue (xs, [])  -> False
 
 -- frontOf :: Queue a -> a
 -- frontOf (Queue (x:xs)) = x
 
--- dequeue :: Queue a -> (a, Queue a)
--- dequeue (Queue xs) = (head xs, Queue $ tail xs)
+dequeue :: Queue a -> (Maybe a, Queue a)
+dequeue q =
+    case q of 
+        Queue ([], []) -> (Nothing, q)
+        Queue (ins, []) -> (Just $ head ins, Queue ([], reverse $ tail ins))
+        Queue (ins, outs) -> (Just $ head outs, Queue (ins, tail outs))
 
--- enqueue :: a -> Queue a -> Queue a
--- enqueue x (Queue xs) = Queue (xs ++ [x])
+
+enqueue :: a -> Queue a -> Queue a
+enqueue x (Queue (ins, outs)) = Queue (x:ins, outs)
